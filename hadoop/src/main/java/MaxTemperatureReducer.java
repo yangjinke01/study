a8b892fa2e4c98241705b9f917feaca4aa339178
yangjinke <yangjinke@chinasoftinc.com>
2019-08-30 09:36:33 +0800

    initial commit

---
+++ hadoop/src/main/java/MaxTemperatureReducer.java
@@ -0,0 +1,17 @@
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+import java.io.IOException;
+
+public class MaxTemperatureReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+    @Override
+    //在shuffle的过程中已经已经变为{a:[1,2,3,4]}的形式了
+    protected void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+        int maxValue = Integer.MIN_VALUE;
+        for (IntWritable value : values) {
+            maxValue = Math.max(maxValue, value.get());
+        }
+        context.write(key, new IntWritable(maxValue));
+    }
+}
